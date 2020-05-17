b4f885b4266993fab4ec751a36792241d01c5121
SharathTUdelft <43334570+SharathTUdelft@users.noreply.github.com>
2020-05-17 21:51:20 +0530

    Added generic domain

--- abstract.py
+++ abstract.py
@@ -1,3 +1,4 @@
+import numpy as np
 from utils import lazy, Singleton
 
 
@@ -18,9 +19,48 @@ from utils import lazy, Singleton
 #     def domain_barrier_edge(self):
 #         raise NotImplementedError
 
+class Domain(Singleton):
+
+    def __init__(self, height=100, width=200):
+        self.height = height
+        self.width = width
+
+    @lazy
+    def domain(self):
+        return np.zeros((self.height, self.width), dtype=bool)
+
+    @lazy
+    def domain_topwall(self):
+        d_top = self.domain().copy()
+        d_top[0, :] = True
+        return d_top
+
+    @lazy
+    def domain_botwall(self):
+        d_top = self.domain().copy()
+        d_top[-1, :] = True
+        return d_top
+
+    @lazy
+    def domain_barrier(self):
+        pass
+
+    @lazy
+    def domain_barrier_edge(self):
+        _domain = self.domain_barrier().copy()  # work with copy as self.domain_barrier is lazy itself
+        x_cord_list = []
+        y_cord_list = []
+        for x_cord in range(1, _domain.shape[1]):
+            for y_cord in range(1, _domain.shape[0]):
+                if _domain[y_cord - 1, x_cord] and _domain[y_cord, x_cord - 1]:
+                    if _domain[y_cord + 1, x_cord] and _domain[y_cord, x_cord + 1]:
+                        x_cord_list.append(x_cord)
+                        y_cord_list.append(y_cord)
+        _domain[y_cord_list, x_cord_list] = False
+        return _domain
+
 
 class Barrier:
-
 
     def apply_barrier(self):
         raise NotImplementedError
