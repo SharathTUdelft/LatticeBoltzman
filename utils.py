32a6daecd70719aa865deb3d177420a7d76d6b12
SharathTUdelft <43334570+SharathTUdelft@users.noreply.github.com>
2020-05-17 17:40:14 +0530

    added comments to singleton

--- utils.py
+++ utils.py
@@ -21,7 +21,7 @@ class Singleton:
 
     def __new__(cls):
         if cls._instance is None:
-            print('Creating the object')
+            print('Creating the singleton domain object')
             cls._instance = super(Singleton, cls).__new__(cls)
             # Put any initialization here.
         return cls._instance
