a0006967e68e9626fa91cc93a2847140456ebadd
SharathTUdelft <43334570+SharathTUdelft@users.noreply.github.com>
2020-05-17 18:48:22 +0530

    implemented domain with circle strategy

---
+++ contrib.py
@@ -0,0 +1,63 @@
+import numpy as np
+from utils import lazy, Singleton
+from barriers import CircleBarrier
+
+
+class Domain(Singleton):
+
+    def __init__(self, height=100, width=200):
+        self.height = height
+        self.width = width
+
+    @lazy
+    def domain(self):
+        return np.zeros((self.height, self.width), dtype=bool)
+
+    @lazy
+    def domain_topwall(self):
+        d_top = self.domain().copy()
+        d_top[0, :] = True
+        return d_top
+
+    @lazy
+    def domain_botwall(self):
+        d_top = self.domain().copy()
+        d_top[-1, :] = True
+        return d_top
+
+    @lazy
+    def domain_barrier(self):
+        position = (int(3 * self.width / 4), int(self.height / 2))
+        return CircleBarrier(domain=self.domain().copy(), position=position, radius=20, height =self.height, width = self.width ).apply_barrier()
+
+    @lazy
+    def domain_barrier_edge(self):
+        _domain = self.domain_barrier().copy()  # work with copy as self.domain_barrier is lazy itself
+        x_cord_list = []
+        y_cord_list = []
+        for x_cord in range(1, _domain.shape[1]):
+            for y_cord in range(1, _domain.shape[0]):
+                if _domain[y_cord - 1, x_cord] and _domain[y_cord, x_cord - 1]:
+                    if _domain[y_cord + 1, x_cord] and _domain[y_cord, x_cord + 1]:
+                        x_cord_list.append(x_cord)
+                        y_cord_list.append(y_cord)
+        _domain[y_cord_list, x_cord_list] = False
+        return _domain
+
+
+
+
+
+
+# Tests
+if __name__ == "__main__":
+    domain = Domain()
+    a1 = domain.domain()
+
+    a2 = domain.domain_topwall()
+    a3 = domain.domain_botwall()
+
+    a4 = domain.domain_barrier()
+    a5 = domain.domain_barrier_edge()
+
+print("debug")
